7b876ff40a53e04b06b87ea28bcddb7edc806a25
Rodrigo Encine <rodrigoencine@hotmail.com>
2019-04-14 15:12:56 -0300

    Alteração classe hash

--- src/br/pucrs/distribuida/p2p/Hash.java
+++ src/br/pucrs/distribuida/p2p/Hash.java
@@ -1,13 +1,29 @@
 package br.pucrs.distribuida.p2p;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Hash {
 
-    public static byte[] hashMessage(String message) throws NoSuchAlgorithmException {
-        byte[] messageBytes = message.getBytes();
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-        messageDigest.update(messageBytes);
-        return messageDigest.digest();
+    public static String hashMessage(File file) throws NoSuchAlgorithmException, FileNotFoundException {
+        try {
+        	MessageDigest digest = MessageDigest.getInstance("MD5");
+        	FileInputStream fileIn = new FileInputStream(file);
+        	DigestInputStream digestIn = new DigestInputStream(fileIn, digest);
+        	
+        	 // Converte bytes para hex
+            StringBuilder result = new StringBuilder();
+            for (byte b : digestIn.getMessageDigest().digest()) {
+                result.append(String.format("%02x", b));
+            }
+            return result.toString();
+        } catch(NoSuchAlgorithmException ex){
+        	System.err.println(ex.getMessage());
+      }
+		return null;
     }
 }
+
